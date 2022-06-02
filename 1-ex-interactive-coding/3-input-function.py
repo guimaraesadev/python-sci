ec5382080a808f4c57d18c182261cf28d83b1235
Sabrina S. Guimarães <guimaraesadev@gmail.com>
2022-06-02 17:02:01 -0300

    input functions + ex

---
+++ 1-ex-interactive-coding/3-input-function.py
@@ -0,0 +1,7 @@
+# Write a program that prints the number of characters in a user's name.
+# You might need to Google for a function that calculates the length of a string.
+
+#input: sabrina #output: 7 
+
+input("Whats your name?")
+
---
+++ day1-var-data-manage/input-function.py
@@ -0,0 +1,10 @@
+#input("A prompt for the user")
+#input("What's your name?")
+
+print("Hello " + input("What's your name?"))
+
+#final output: Hello Sabrina. 
+
+
+
+
