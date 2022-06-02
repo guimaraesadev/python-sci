ae6cc93d9e173dfedcce5261f7c6025d873a34ad
Sabrina S. Guimarães <guimaraesadev@gmail.com>
2022-06-02 16:48:09 -0300

    🐞 debbuding initial code + strings test

--- 1-ex-interactive-coding/1-printing.py
+++ 1-ex-interactive-coding/1-printing.py
@@ -2,5 +2,20 @@
 
 print("Day 1 - Python Print Function")
 print("The function is declared like this:")
-print("print('what to print')")
+print("print('what to print')") #i used single quotes to avoid conflict.
+
+#for skip lines use \n 
+
+print("Hello! \nHow Are you?")
+print(" Hi. \n My name is Sabrina! \n And you?")
+
+#concatenate 
+
+print("Hello" + " " + "Sabrina") 
+
+
+
+
+
+
 
