ef031626d9e7ac0e90d99d1545606d1a821c6784
Sabrina S. Guimarães <guimaraesadev@gmail.com>
2022-06-02 17:58:22 -0300

    adding day 1 finalization project

---
+++ day1-var-data-manage/project-day-1/band-name-generator.py
@@ -0,0 +1,23 @@
+# create a greeting for your program
+from unicodedata import name
+
+
+print("Welcome to the better band name generator!\n")
+# ask the user for the city that they grew up
+city_growUp = input("Which city did you grow up in?\n")
+# ask the user for the name of a pet
+name_pet = input("What's the name of your pet?\n")
+# combine the nome of their city and pet and show them their band name
+print("Your band name could be: " + city_growUp + " " + name_pet)
+
+
+
+
+
+
+
+
+
+
+
+
